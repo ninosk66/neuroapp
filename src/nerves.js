7c320192eb094f8f36d1e4d87070366b5874d1e2
Ninos <ninoskossa@hotmail.com>
2023-04-26 13:08:47 -0700

    initial commit

---
+++ src/nerves.js
@@ -0,0 +1,61 @@
+export const nerves = [
+  {
+    id: 1,
+    level: "C1",
+    muscles: [
+      "C1: Rectus capitis posterior minor",
+      "C1:Rectus capitis posterior major",
+    ],
+  },
+  {
+    id: 2,
+    level: "C2",
+    muscles: [
+      "C2: Rectus capitis posterior major",
+      "C2: Obliquus capitis superior",
+      "C2: Splenius capitis",
+    ],
+  },
+  {
+    id: 3,
+    level: "C3",
+    muscles: ["C3: Trapezius (upper fibers)", "C3: Levator scapulae"],
+  },
+  { id: 4, level: "C4", muscles: ["C4: Trapezius", "C4: Levator Scapulae"] },
+  { id: 5, level: "C5", muscles: ["C5: Deltoid", "C5: Biceps"] },
+  {
+    id: 6,
+    level: "C6",
+    muscles: ["C6: Wrist Extensors", "C6: Biceps", "C6: Supinator"],
+  },
+  { id: 7, level: "C7", muscles: ["C7: Triceps", "C7: Wrist Flexors"] },
+  {
+    id: 8,
+    level: "C8",
+    muscles: ["C8: Finger Flexors", "C8: Abductor Pollicis"],
+  },
+  { id: 9, level: "T1-T12", muscles: ["T1-T12: Intercostal Muscles"] },
+  { id: 10, level: "L1", muscles: ["L1: Iliopsoas", "L1: Quadriceps"] },
+  { id: 11, level: "L2", muscles: ["L2: Hip Adductors", "L2: Hamstrings"] },
+  { id: 12, level: "L3", muscles: ["L3: Quadriceps", "L3: Adductor Magnus"] },
+  { id: 13, level: "L4", muscles: ["L4: Tibialis Anterior", "L4: Quadriceps"] },
+  {
+    id: 14,
+    level: "L5",
+    muscles: ["L5: Extensor Hallucis Longus", "L5: Gluteus Medius"],
+  },
+  {
+    id: 15,
+    level: "S1",
+    muscles: ["S1: Gastrocnemius", "S1: Gluteus Maximus"],
+  },
+  {
+    id: 16,
+    level: "S2-S5",
+    muscles: [
+      "S2-S5: Levator Ani",
+      "S2-S5: External Anal Sphincter",
+      "S2-S5: Internal Anal Sphincter",
+    ],
+  },
+];
