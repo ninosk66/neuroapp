2c687d21456548ec92bf2a9de16db5c802e07a01
Ninos <ninoskossa@hotmail.com>
2023-04-26 21:25:31 -0700

    updated transition effect

--- src/nerveSelector.js
+++ src/nerveSelector.js
@@ -5,56 +5,55 @@ function NerveSelector({ nerves }) {
   const [level1, setLevel1] = useState("");
   const [level2, setLevel2] = useState("");
   const [muscles, setMuscles] = useState([]);
-  const [isClicked, setIsClicked] = useState(false)
+  const [isClicked, setIsClicked] = useState(false);
   const handleLevel1Change = (e) => {
-      setLevel1(e.target.value);
-      setIsClicked(false)
+    setLevel1(e.target.value);
+    setIsClicked(false);
   };
 
   const handleLevel2Change = (e) => {
-      setLevel2(e.target.value);
-      setIsClicked(false)
+    setLevel2(e.target.value);
+    setIsClicked(false);
   };
 
-    const handleShowResult = () => {
-        setIsClicked(true); 
-        console.log(isClicked)
+  const handleShowResult = () => {
+    setIsClicked(true);
+    console.log(isClicked);
     // const muscleGroups = [];
-    const start = nerves.findIndex((n) => n.level === level1);;
-      const end = nerves.findIndex((n) => n.level === level2);;
-      
+    const start = nerves.findIndex((n) => n.level === level1);
+    const end = nerves.findIndex((n) => n.level === level2);
 
     if (start === -1 || end === -1) {
       alert("Please select valid levels");
       return;
-        }
-        
-        const muscles = nerves
-        .slice(start, end + 1)
-        .flatMap((nerve) => nerve.muscles)
-        .filter((muscle) => muscle !== "None");
-  
-      setMuscles(muscles);
-    };
+    }
 
-//     for (let i = start; i <= end; i++) {
-//       nerves[i].muscles.forEach((muscle) => {
-//         if (!muscleGroups.includes(muscle)) {
-//           muscleGroups.push(muscle);
-//         }
-//       });
-//     }
+    const muscles = nerves
+      .slice(start, end + 1)
+      .flatMap((nerve) => nerve.muscles)
+      .filter((muscle) => muscle !== "None");
 
-//     setMuscles(muscleGroups);
-//   };
+    setMuscles(muscles);
+  };
+
+  //     for (let i = start; i <= end; i++) {
+  //       nerves[i].muscles.forEach((muscle) => {
+  //         if (!muscleGroups.includes(muscle)) {
+  //           muscleGroups.push(muscle);
+  //         }
+  //       });
+  //     }
+
+  //     setMuscles(muscleGroups);
+  //   };
 
   return (
-    <div className="container">
+    <div className='container'>
       <h2>Nerve Selector</h2>
-      <div className="select-container">
-        <label htmlFor="level1">Select Level 1:</label>
-        <select id="level1" value={level1} onChange={handleLevel1Change}>
-          <option value="">--Please choose a level--</option>
+      <div className='select-container'>
+        <label htmlFor='level1'>Select Level 1:</label>
+        <select id='level1' value={level1} onChange={handleLevel1Change}>
+          <option value=''>--Please choose a level--</option>
           {nerves.map((nerve) => (
             <option key={nerve.ID} value={nerve.level}>
               {nerve.level}
@@ -62,10 +61,10 @@ function NerveSelector({ nerves }) {
           ))}
         </select>
       </div>
-      <div className="select-container">
-        <label htmlFor="level2">Select Level 2:</label>
-        <select id="level2" value={level2} onChange={handleLevel2Change}>
-          <option value="">--Please choose a level--</option>
+      <div className='select-container'>
+        <label htmlFor='level2'>Select Level 2:</label>
+        <select id='level2' value={level2} onChange={handleLevel2Change}>
+          <option value=''>--Please choose a level--</option>
           {nerves.map((nerve) => (
             <option key={nerve.ID} value={nerve.level}>
               {nerve.level}
@@ -73,19 +72,22 @@ function NerveSelector({ nerves }) {
           ))}
         </select>
       </div>
-      <div className="button-container">
-        <button className="show-result-button" onClick={handleShowResult}>
+      <div className='button-container'>
+        <button className='show-result-button' onClick={handleShowResult}>
           Show Result
         </button>
       </div>
       {muscles.length > 0 && (
-        <div className="result-container">
+        <div className='result-container'>
           <h3>Result:</h3>
           <ul>
             {muscles.map((muscle) => (
-                <li
-                    // className={isClicked ? "list-item-show" : 'list-item'} 
-                    key={muscle}>{muscle}</li>
+              <li
+                className={isClicked ? "list-item-show" : "list-item"}
+                key={muscle}
+              >
+                {muscle}
+              </li>
             ))}
           </ul>
         </div>
